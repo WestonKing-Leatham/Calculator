be7023c380eb805f2e6ba75f0ff8129c8f988579
Weston King-Leatham <westonkingl1@gmail.com>
2020-11-13 19:58:41 -0500

    Finished evaluation method

--- my_calculator/src/App.js
+++ my_calculator/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import './App.css';
 import Button from './components/Button';
 import Input from './components/Input';
-import ClearButton from "./components/ClearButton";
+import ClearButton from './components/ClearButton';
 
 class App extends Component {
   constructor(props) {
@@ -19,7 +19,7 @@ class App extends Component {
   addToInput = val => {
     this.setState({ input: this.state.input + val });
   };
-  
+
   addDecimal = val => {
     // only add decimal if there is no current decimal point present in the input area
     if (this.state.input.indexOf(".") === -1) {
@@ -62,6 +62,36 @@ class App extends Component {
     this.state.operator = "divide";
   };
 
+  evaluate = () => {
+    this.state.currentNumber = this.state.input;
+
+    if (this.state.operator == "plus") {
+      this.setState({
+        input:
+          parseFloat(this.state.previousNumber) +
+          parseFloat(this.state.currentNumber)
+      });
+    } else if (this.state.operator == "subtract") {
+      this.setState({
+        input:
+        parseFloat(this.state.previousNumber) -
+        parseFloat(this.state.currentNumber)
+      });
+    } else if (this.state.operator == "multiply") {
+      this.setState({
+        input:
+        parseFloat(this.state.previousNumber) *
+        parseFloat(this.state.currentNumber)
+      });
+    } else if (this.state.operator == "divide") {
+      this.setState({
+        input:
+        parseFloat(this.state.previousNumber) /
+        parseFloat(this.state.currentNumber)
+      });
+    }
+  };
+
 render() {
   return (
     <div className="App">
@@ -100,5 +130,6 @@ render() {
       </div>
   );
 }
+
 }
 export default App;
